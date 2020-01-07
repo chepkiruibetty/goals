a5eea3e396c748fccca99a2d679459f66b968037
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-07 10:47:39 +0300

    modified

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -14,9 +14,16 @@ export class GoalComponent implements OnInit {
     new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon'),
     new Goal(4,'Get Dog Food','Pupper likes expensive snacks'),
     new Goal(5,'Solve math homework','Damn Math'),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord'),
+    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord'), 
   ];
-
+  toggleDetails(index){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+  completeGoal(isComplete, index){
+    if (isComplete) {
+      this.goals.splice(index,1);
+    }
+  }
   constructor() { }
 
   ngOnInit() {
