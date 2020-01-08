35c100c526e17cbb55ebc883e3d82b3be4b7e2be
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-08 11:13:39 +0300

    modify my files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
@@ -16,8 +17,7 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
     GoalDetailComponent,
     StrikethroughDirective,
     DateCountPipe,
-    GoalFormComponent,
-
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
